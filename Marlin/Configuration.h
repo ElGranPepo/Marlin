e7698c0245d8aba711dd079234c5478a0d50267c
vertabreaker <opyrus@hotmail.com>
2020-02-12 18:11:43 -0500

    tweak

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1338,7 +1338,7 @@
 
 // For M851 give a range for adjusting the Z probe offset
 #define Z_PROBE_OFFSET_RANGE_MIN -20
-#define Z_PROBE_OFFSET_RANGE_MAX  20
+#define Z_PROBE_OFFSET_RANGE_MAX  0
 
 // Enable the M48 repeatability test to test probe accuracy
 //#define Z_MIN_PROBE_REPEATABILITY_TEST
